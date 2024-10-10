8868442a005799c4d2810baeef4a85f55e4545fe
cyrus_w <cyrusw89@gmail.com>
2024-10-10 18:26:48 +1300

    add: chip section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -34,7 +34,7 @@ export default function Features() {
   }, []);
 
   return (
-    <section className="h-full common-padding bg-sinc relative overflow-hidden">
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-width">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">
